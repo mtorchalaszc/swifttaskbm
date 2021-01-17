08319374ff8cb52212a2746c62e728a01da67ebc
mtzgk <mtz_gk@yahoo.pl>
2021-01-17 16:20:36 +0000

    all basic files

---
+++ Checklist.swift
@@ -0,0 +1,14 @@
+class Checklist {
+
+var remoteTodos: [[String : Any]] = [["id": 1, "title": "Create SW Requirements", "completed": 0],
+                                     ["id": 2, "title": "SWR Review", "completed": 0],
+                                     ["id": 3, "title": "SWT Task 1", "completed": 1],
+                                     ["id": 4, "title": "SWT Task 1 Review 1", "completed": 1],
+                                     ["id": 5, "title": "SWD Task 1", "completed": 0],
+                                     ["id": 6, "title": "SWD Task 1 Review 1", "completed": 0],
+                                     ["id": 7, "title": "SWD Task 2", "completed": 0],
+                                     ["id": 8, "title": "SWD Task 2 Review 1", "completed": 1],
+                                     ["id": 9, "title": "SWD Task 2 Review 2", "completed": 0],
+                                     ["id": 10, "title": "SWT Task 2", "completed": 1]]
+
+}
---
+++ TodoViewController.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+class TodoViewControler {
+    
+    //funkcja imitujaca interfejs zwracania textu z textfield
+    func fillImaginaryTextField(with text: String?) {
+        
+    }
+    
+    //funkcja imitujaca interfejs przelaczenia checkbox
+    func toggleImaginaryCheckbox() {
+
+    }
+    
+    //funkcja imitujaca przycisniecie przycisku AddNewToDo
+    func imaginaryButtonActionAddNewToDo() {
+
+    }
+    
+    //funkcja imitujaca przycisniecie przycisku RemoveTodo dla obiektu z id
+    func imaginaryButtonActionRemoveTodo(with id: Int) {
+
+    }
+    
+    //funkcja imitujaca przelaczenie checkbox dla obiektu z id
+    func imaginaryButtonActionToggleTodo(with id: Int) {
+
+    }
+}
